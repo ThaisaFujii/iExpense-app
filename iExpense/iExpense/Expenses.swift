8906fa1f0771858b6d4b1eaec46e351a22cb9c15
ThaisaFujii <71739802+ThaisaFujii@users.noreply.github.com>
2022-06-29 14:32:12 -0300

    commit comments
    new comments about methods and logic.

--- iExpense/iExpense/Expenses.swift
+++ iExpense/iExpense/Expenses.swift
@@ -9,16 +9,21 @@ import Foundation
 
 class Expenses: ObservableObject {
     @Published var items = [ExpenseItem](){
+        
         didSet {
-            //means “create an encoder and use it to encode something"
+            //converting our data to JSON and use that json in our view
             let encoder = JSONEncoder()
             if let encoded = try? encoder.encode(items) {
                 UserDefaults.standard.set(encoded, forKey: "Items")
             }
         }
     }
+    // init para pegar o input feito pelo usuario
     init() {
+        // decoder let us go from JSON data to Swift objects
+        // attempts to read whatever is in “Items” as a Data object
         if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            //does the actual job of unarchiving the Data object into an array of ExpenseItem objects.
             if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
                 items = decodedItems
                 return
